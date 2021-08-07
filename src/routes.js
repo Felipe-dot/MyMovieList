aa7038db26c4722c021b162ed4cc7f04da4418c7
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2021-08-07 16:49:35 -0300

    Added migrations settings

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
-import { Router } from 'express'; // importr apenas o Routers do express
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ mensagem: 'helo world' }));
+routes.post('/user', async (req, res) => {
+  const user = await User.create({
+    name: 'Luis Felipe de Azevedo Melo',
+    nickname: 'Felipe',
+    email: 'felipeluis@gmail.com',
+    password_hash: '123456',
+  });
+  res.json(user);
+});
+
+routes.get('/', (req, res) => res.json({ mensagem: 'hello world' }));
 
 export default routes;
