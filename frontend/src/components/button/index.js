04688fefd7deb1733d205b98ab6bbebcd01c109b
Carlos Eduardo <dudu_carlos.eduardo@hotmail.com>
2021-09-24 11:06:03 -0300

    a

--- frontend/src/components/button/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Container } from "./styles";
-
-function Button({ children, ...rest }) {
-  return <Container {...rest}>{children}</Container>;
-}
-
-export default Button;
